95eab20961155b4ae870873af9a571eac85ecfa5
MyNameIsN0body <homer_xxx86@mail.ru>
2020-03-20 16:04:58 +0400

    create
    create Main, Staff, StaffBuilder

---
+++ model/Account.java
@@ -0,0 +1,13 @@
+package voda.model;
+
+public class Account {
+    private final double balance;
+
+    public Account(double balance) {
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
